9ac52a300bdbae215690f66c83fcf16f9c40809b
Ovidiu Chereches <skidding@gmail.com>
2013-05-09 13:00:06 +0300

    Moved lines around in start script #22

--- script/start.sh
+++ script/start.sh
@@ -8,6 +8,7 @@ then
   port=$1
 fi
 
-PORT=$port MONGO_URL=mongodb://aufond:aufond.mongodb@dharma.mongohq.com:10042/aufond \
+PORT=$port \
+MONGO_URL=mongodb://aufond:aufond.mongodb@dharma.mongohq.com:10042/aufond \
 ROOT_URL=http://aufond.me:$port \
 node bundle/main.js
