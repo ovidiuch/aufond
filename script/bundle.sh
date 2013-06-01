11c29aee56909c39d0e03f856a4e23784e359310
Ovidiu Chereches <skidding@gmail.com>
2013-06-01 11:03:49 +0300

    Move bundle in ".bundle" folder
    In order for it to be ignored by Meteor in development

--- script/bundle.sh
+++ script/bundle.sh
@@ -1,19 +1,28 @@
 #/bin/bash
 
 # Clear previous bundle (if any)
-if [ -d "bundle/" ]
+if [ -d ".bundle" ]
 then
-  rm -r bundle/
+  echo "Removing previous .bundle..."
+  rm -r .bundle/*
+else
+  echo "Creating .bundle folder..."
+  mkdir .bundle
 fi
 
 # Create bundle
+echo "Creating Meteor bundle file..."
 meteor bundle aufond.tgz
 
-# Unzip bundle in bundle/ folder
-tar xzf aufond.tgz bundle/
+# Unzip bundle in .bundle/ folder
+# --strip-components removes root "bundle" folder from archive
+# -C dumps it into a different folder than the current one
+echo "Unzipping bundle in .bundle folder..."
+tar xzf aufond.tgz --strip-components=1 -C .bundle
 
 # Fix node fibers package
-cd bundle/server
+echo "Re-install fibers package in .bundle folder..."
+cd .bundle/server
 npm uninstall fibers
 npm install fibers
 cd -
--- script/start.sh
+++ script/start.sh
@@ -11,4 +11,4 @@ fi
 PORT=$port \
 MONGO_URL=mongodb://aufond:aufond.mongodb@dharma.mongohq.com:10042/aufond \
 ROOT_URL=http://aufond.me:$port \
-node bundle/main.js
+node .bundle/main.js
