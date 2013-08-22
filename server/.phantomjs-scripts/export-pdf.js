f61f156441f3b7b1925f76018d03bf8bef4ded63
Ovidiu Chereches <skidding@gmail.com>
2013-08-22 22:26:57 +0300

    Remove google web fonts external link from page content before pdf export #67

--- server/.phantomjs-scripts/export-pdf.js
+++ server/.phantomjs-scripts/export-pdf.js
@@ -1,3 +1,6 @@
+function removeGoogleFontsLink(content) {
+    return content.replace(/<link href=".+?fonts\.googleapis\.com.+?".+?>/, '');
+}
 function resetHeightInContent(content) {
     return content.replace(/class="content" style=".+?"/,
                            'class="content" style="height: auto;"');
@@ -17,9 +20,15 @@ page.open(url, function (status) {
         console.log('Unable to load the address ' + url + '!');
         phantom.exit();
     } else {
+        var alteredContent = page.content;
+        // XXX PhantomJS does not yet work with Google Web Fonts, so we need to
+        // remove the external link altogether. They need to be installed on
+        // the local machine in order to work
+        alteredContent = removeGoogleFontsLink(alteredContent);
         // XXX the print display for the contact links differs in height so the
         // already set height for the container from JS logic must be removed
-        page.setContent(resetHeightInContent(page.content), url);
+        alteredContent = resetHeightInContent(alteredContent);
+        page.setContent(alteredContent, url);
         window.setTimeout(function () {
             page.render(output);
             phantom.exit();
